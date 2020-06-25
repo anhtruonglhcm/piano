4746a410257b275cfdd35a2742a251b219c79acc
Le van Truong <nhungtruongforever@gmail.com>
2020-06-25 11:00:17 +0700

    first-piano

---
+++ precache-manifest.b3b81591a12031b964bacb3a8b15307a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "051ca50a3e7379383fef03cd49e6d9aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3982c2e29177a33b7a72",
+    "url": "/static/css/main.dc3e9b98.chunk.css"
+  },
+  {
+    "revision": "12a9b11ccb23c0ea696c",
+    "url": "/static/js/2.ff0cb6a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ff0cb6a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3982c2e29177a33b7a72",
+    "url": "/static/js/main.4b2e22a9.chunk.js"
+  },
+  {
+    "revision": "49de1d56170d62cf3751",
+    "url": "/static/js/runtime-main.d0798475.js"
+  }
+]);
